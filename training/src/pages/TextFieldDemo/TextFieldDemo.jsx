f86a0b6d9c4ca42121c0096d8441785814183f20
Akhilesh Kumbhkar <akhilesh.kumbhkar@akhileshr273.successivetech.com>
2020-12-11 21:15:50 +0530

    React - Introduction to State

--- training/src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ training/src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+
 import { TextField } from '../../components';
 
-function TextFieldDemo() {
-  return (
-    <>
-      <TextField disabled />
-      <br />
-      <TextField value="Accessible" />
-      <br />
-      <TextField error="101" />
-      <br />
-    </>
-  );
-}
+import { Slider } from '../../components/slider';
+
+import { banners } from '../../config/constants';
+
+import { Div } from '../../components/TextField/style';
+
+const TextFieldDemo = () => (
+  <Div>
+    <div>
+      <Slider alt="No Image" duration="1000" height="300" random banner={banners} />
+    </div>
+    <p><b>This is a Disabled Input</b></p>
+    <TextField disabled value="disabled input" />
+    <p><b> A Valid Input</b></p>
+    <TextField value="accessible" />
+    <p><b>An input with Errors </b></p>
+    <TextField error="Could not be more than" value="101" />
+  </Div>
+);
 
 export default TextFieldDemo;
