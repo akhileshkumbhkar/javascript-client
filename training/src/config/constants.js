ad7cb3a4a614de66f4a0b5be5abcfef6ff372960
Akhilesh Kumbhkar <akhilesh.kumbhkar@akhileshr273.successivetech.com>
2020-12-20 17:27:50 +0530

    React - NavBar and Login UI

--- training/src/config/constants.js
+++ training/src/config/constants.js
@@ -46,5 +46,12 @@ const radioOptionsFootball = [
     value: 'defender',
   },
 ];
+const schema = yup.object().shape({
+  email: yup.string()
+    .trim().email().required('Email Address is a required field'),
+  password: yup.string()
+    .required('Password is required')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/, 'Must contain 8 characters, at least one uppercase letter, one lowercase letter and one number'),
+});
 
-export { radioOptionsCricket, radioOptionsFootball, selectOptions };
+export { radioOptionsCricket, radioOptionsFootball, selectOptions,schema };
