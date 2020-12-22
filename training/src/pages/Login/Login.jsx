af4742800e8c12242072a27cc6ea7f938d4e4f8b
Akhilesh Kumbhkar <akhilesh.kumbhkar@akhileshr273.successivetech.com>
2020-12-22 20:30:53 +0530

    React - Basic Route Setup and Layouts

--- training/src/pages/Login/Login.jsx
+++ training/src/pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -136,9 +137,7 @@ class Login extends React.Component {
       );
     }
 }
-
 Login.propTypes = {
   classes: PropTypes.objectOf(PropTypes.string).isRequired,
 };
-
 export default withStyles(Design)(Login);
