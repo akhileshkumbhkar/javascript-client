9b03c670846528b33ec03df45a2bfc3a481565e7
Akhilesh Kumbhkar <akhilesh.kumbhkar@akhileshr273.successivetech.com>
2020-12-24 17:37:15 +0530

    React - Dynamic Routing

--- training/src/pages/Trainee/Trainee.jsx
+++ training/src/pages/Trainee/Trainee.jsx
@@ -1,41 +1,19 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import AddDialog from './components/AddDialog/AddDialog';
+import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+import TraineeList from './TraineeList';
+import TraineeDetail from './TraineeDetail';
 
-export default class Trainee extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false,
-    };
-  }
-
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    const { open } = this.state;
-    this.setState({ open: false });
-    return open;
-  };
-
-  handleSubmit = (data) => {
-    this.setState({
-      open: false,
-    }, () => {
-      console.log(data);
-    });
-  }
-
-  render() {
-    const { open } = this.state;
-    return (
-      <>
-        <br />
-        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>ADD TRAINEE</Button>
-        <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
-      </>
-    );
-  }
+function Trainee(props) {
+  const { match: { path } } = props;
+  return (
+    <Switch>
+      <Route exact path={path} component={TraineeList} />
+      <Route exact path={`${path}/:traineeId`} component={TraineeDetail} />
+    </Switch>
+  );
 }
+Trainee.propTypes = {
+  match: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+export default Trainee;
