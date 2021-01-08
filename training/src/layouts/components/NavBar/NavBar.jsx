3e66d716949caacb23bb1769b34499ecffa0044c
Akhilesh Kumbhkar <akhilesh.kumbhkar@akhileshr273.successivetech.com>
2021-01-08 13:37:10 +0530

    Spinner, Axios login and routes authorize

--- training/src/layouts/components/NavBar/NavBar.jsx
+++ training/src/layouts/components/NavBar/NavBar.jsx
@@ -19,24 +19,30 @@ const useStyles = makeStyles((theme) => ({
 
 function NavBar() {
   const classes = useStyles();
+
   return (
-    <>
-      <div className={classes.root} style={{ margin: 0 }}>
-        <CssBaseline />
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              Trainee Portal
-            </Typography>
-            <Button component={Link} to="/Trainee" color="inherit">TRAINEE</Button>
-            <Button component={Link} to="/TextFieldDemo" color="inherit">TEXTFIELD DEMO</Button>
-            <Button component={Link} to="/InputDemo" color="inherit">INPUT DEMO</Button>
-            <Button component={Link} to="/ChildrenDemo" color="inherit">CHILDREN DEMO</Button>
-            <Button color="inherit" className={classes.logout}>LOGOUT</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </>
+    <div className={classes.root} style={{ margin: 0 }}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Trainee Portal
+          </Typography>
+          <Button component={Link} to="/Trainee" color="inherit">TRAINEE</Button>
+          <Button component={Link} to="/TextFieldDemo" color="inherit">TEXTFIELD DEMO</Button>
+          <Button component={Link} to="/InputDemo" color="inherit">INPUT DEMO</Button>
+          <Button component={Link} to="/ChildrenDemo" color="inherit">CHILDREN DEMO</Button>
+          <Button
+            color="inherit"
+            style={{ marginLeft: 30 }}
+            href="/login"
+            onClick={() => { localStorage.clear('token'); }}
+          >
+            LOGOUT
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 export default NavBar;
--- training/src/pages/Login/Login.jsx
+++ training/src/pages/Login/Login.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable */
 import React from 'react';
+import * as yup from 'yup';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import {
-  TextField, Typography, CardContent, InputAdornment, Button, Avatar, Card, CssBaseline, withStyles,
+  TextField, CssBaseline, Card, Typography, Avatar,
+  CardContent, withStyles, InputAdornment, Button, CircularProgress,
 } from '@material-ui/core';
-import { LockOutlined } from '@material-ui/icons';
-import { schema } from '../../config/constants'
-import { Email, VisibilityOff } from '@material-ui/icons';
+import { Email, VisibilityOff, LockOutlined } from '@material-ui/icons';
+import localStorage from 'local-storage';
+import callApi from '../../libs/utils/api';
+import { MyContext } from '../../contexts/snackBarProvider/index';
 
+const schema = yup.object().shape({
+  email: yup.string()
+    .trim().email().required('Email Address is a required field'),
+  password: yup.string()
+    .required('Password is required'),
+});
 const Design = (theme) => ({
   icon: {
     background: 'red',
@@ -19,14 +29,20 @@ const Design = (theme) => ({
     marginTop: theme.spacing(25),
     marginLeft: theme.spacing(50),
   },
-})
-
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
 class Login extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       email: '',
       password: '',
+      loading: false,
+      redirect: false,
+      hasError: true,
+      message: '',
       touched: {
         email: false,
         password: false,
@@ -34,18 +50,54 @@ class Login extends React.Component {
     };
   }
 
-  handleChange = (key) => ({ target: { value } }) => {
-    this.setState({ [key]: value });
-  };
-
-    hasErrors = () => {
-      try {
-        schema.validateSync(this.state);
-      } catch (err) {
-        return true;
+    renderRedirect = () => {
+      const { redirect } = this.state;
+      if (redirect) {
+        return <Redirect to="/trainee" />;
       }
-      return false;
     }
+
+    handleChange = (key) => ({ target: { value } }) => {
+      this.setState({ [key]: value });
+    };
+
+    onClickHandler = async (data, openSnackBar) => {
+      console.log('Data is :', data);
+      this.setState({
+        loading: true,
+        hasError: true,
+      });
+      await callApi(data, 'post', '/user/login');
+      this.setState({ loading: false });
+      const response = localStorage.get('token');
+      console.log(' res inside login :', response.status);
+      if (response.status === 200) {
+        this.setState({
+          redirect: true,
+          hasError: false,
+          message: 'Successfully Login',
+        }, () => {
+          const { message } = this.state;
+          openSnackBar(message, 'success');
+        });
+      } else {
+        this.setState({
+          message: 'Login Failed, Record Not Found',
+        }, () => {
+          const { message } = this.state;
+          openSnackBar(message, 'error');
+        });
+      }
+    }
+
+  hasErrors = () => {
+    try {
+      schema.validateSync(this.state);
+    } catch (err) {
+      return true;
+    }
+    return false;
+  }
 
     getError = (field) => {
       const { touched } = this.state;
@@ -69,11 +121,15 @@ class Login extends React.Component {
       });
     }
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <>
-         <div className={classes.main}>
+    render() {
+      const { classes } = this.props;
+      const {
+        email, password, loading,
+      } = this.state;
+      this.hasErrors();
+      return (
+        <>
+          <div className={classes.main}>
             <CssBaseline />
             <Card open aria-labelledby="form-dialog-title">
               <Avatar className={classes.icon}>
@@ -84,14 +140,14 @@ class Login extends React.Component {
                 <form>
                   <div>
                     <TextField
-                      helperText={this.getError('email')}
-                      error={!!this.getError('email')}
                       required
+                      fullWidth
                       id="outlined-required"
                       label="Email Address"
-                      defaultValue=" "
+                      defaultValue=""
                       variant="outlined"
-                      fullWidth
+                      helperText={this.getError('email')}
+                      error={!!this.getError('email')}
                       onChange={this.handleChange('email')}
                       onBlur={() => this.isTouched('email')}
                       InputProps={{
@@ -103,17 +159,17 @@ class Login extends React.Component {
                       }}
                     />
                   </div>
-              &nbsp;
+                  <br />
                   <div>
                     <TextField
-                      type="password"
-                      helperText={this.getError('password')}
-                      error={!!this.getError('password')}
                       required
+                      type="password"
+                      fullWidth
                       id="outlined-required"
                       label="Password"
                       variant="outlined"
-                      fullWidth
+                      helperText={this.getError('password')}
+                      error={!!this.getError('password')}
                       onChange={this.handleChange('password')}
                       onBlur={() => this.isTouched('password')}
                       InputProps={{
@@ -125,9 +181,29 @@ class Login extends React.Component {
                       }}
                     />
                   </div>
-              &nbsp;
+                &nbsp;
                   <div>
-                    <Button variant="contained" color="primary" disabled={this.hasErrors()} fullWidth>SIGN IN</Button>
+                    <MyContext.Consumer>
+                      {({ openSnackBar }) => (
+                        <Button
+                          fullWidth
+                          variant="contained"
+                          color="primary"
+                          className={classes.submit}
+                          disabled={this.hasErrors()}
+                          onClick={() => {
+                            this.onClickHandler({ email, password }, openSnackBar);
+                          }}
+                        >
+                          {loading && (
+                            <CircularProgress />
+                          )}
+                          {loading && <span>Signing in</span>}
+                          {!loading && <span>Sign in</span>}
+                          {this.renderRedirect()}
+                        </Button>
+                      )}
+                    </MyContext.Consumer>
                   </div>
                 </form>
               </CardContent>
