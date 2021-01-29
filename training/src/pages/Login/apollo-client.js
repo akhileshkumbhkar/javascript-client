a14c59735073092bd127e927bc8961b963235258
Akhilesh Kumbhkar <akhilesh.kumbhkar@akhileshr273.successivetech.com>
2021-01-29 12:15:36 +0530

    Integration of Apollo Client

--- training/src/pages/Login/apollo-client.js
+++ training/src/pages/Login/apollo-client.js
@@ -6,20 +6,18 @@ import { setContext } from '@apollo/client/link/context';
 const link = new HttpLink({ uri: process.env.REACT_APP_APOLLO_GRAPHQL_URI });
 
 const authLink = setContext((_, { headers }) => {
-// get the authentication token if it's exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httplink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+	const token = localStorage.getItem('token');
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
 });
 
 const Apolloclient = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: authLink.concat(link),
+	cache: new InMemoryCache(),
+	link: authLink.concat(link),
 });
 
 export default Apolloclient;
